9793c877e29216f8493eef49cb3c6d30162e1f5e
Nikita Potorokin <ozornik.xd@mail.ru>
2024-11-11 20:34:52 +0300

    TestController updated

--- src/Common/Infrastructure/Controller/Api/Test/TestController.php
+++ src/Common/Infrastructure/Controller/Api/Test/TestController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Controller;
+namespace App\Common\Infrastructure\Controller\Api\Test;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;

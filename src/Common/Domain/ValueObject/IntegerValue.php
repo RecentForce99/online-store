516f1f8f110b011dcc7d2273a6fd585bc9b14543
Nikita Potorokin <ozornik.xd@mail.ru>
2024-11-13 23:32:19 +0300

    Value Object abstractions created

---
+++ src/Common/Domain/ValueObject/IntegerValue.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\Domain\ValueObject;
+
+abstract class IntegerValue
+{
+    protected function __construct(protected int $value)
+    {
+    }
+
+    public static function fromInt(int $value): static
+    {
+        return new static($value);
+    }
+
+    public function value(): int
+    {
+        return $this->value;
+    }
+
+    public function isEqualTo(self $other): bool
+    {
+        return $this->value() === $other->value();
+    }
+
+    public function isLessThan(self $other): bool
+    {
+        return $this->value() < $other->value();
+    }
+
+    public function isGreaterThan(self $other): bool
+    {
+        return $this->value() > $other->value();
+    }
+}

73075b137515c3ecc97fc652e52a7bfe74503a1b
Nikita Potorokin <ozornik.xd@mail.ru>
2024-11-15 22:49:54 +0300

    BaseEntity updated

--- src/Common/Domain/Entity/AbstractBaseEntity.php
+++ src/Common/Domain/Entity/AbstractBaseEntity.php
@@ -2,12 +2,10 @@
 
 declare(strict_types=1);
 
-namespace App\User\Domain\Entity;
+namespace App\Common\Domain\Entity;
 
-use App\User\Domain\ValueObject\Uuid;
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
-use JsonSerializable;
 use Symfony\Component\Uid\AbstractUid;
 use Symfony\Component\Uid\UuidV4;
 
@@ -16,7 +14,6 @@ abstract class AbstractBaseEntity implements EntityInterface
 {
     #[ORM\Id]
     #[ORM\Column(type: 'uuid', unique: true)]
-    #[ORM\GeneratedValue]
     protected UuidV4 $id;
 
     #[ORM\Column(type: 'datetime_immutable')]

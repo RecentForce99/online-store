7bcfc42e39659a618deabbfac8301a42b9ca39c9
Nikita Potorokin <ozornik.xd@mail.ru>
2024-11-22 19:23:49 +0300

    AbstractBaseEntity reworked

--- src/Common/Domain/Entity/AbstractBaseEntity.php
+++ src/Common/Domain/Entity/AbstractBaseEntity.php
@@ -6,7 +6,6 @@ namespace App\Common\Domain\Entity;
 
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Uid\AbstractUid;
 use Symfony\Component\Uid\UuidV4;
 
 abstract class AbstractBaseEntity
@@ -21,18 +20,35 @@ abstract class AbstractBaseEntity
     #[ORM\Column(type: 'datetime_immutable')]
     protected DateTimeImmutable $updatedAt;
 
-    public function getId(): AbstractUid
+    protected function __construct()
+    {
+        $this->id = new UuidV4();
+    }
+
+    public function getId(): UuidV4
     {
         return $this->id;
     }
 
-    public function getCreatedAt(): ?DateTimeImmutable
+    public function getCreatedAt(): DateTimeImmutable
     {
         return $this->createdAt;
     }
 
-    public function getUpdatedAt(): ?DateTimeImmutable
+    public function setCreatedAt(DateTimeImmutable $createdAt): AbstractBaseEntity
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function getUpdatedAt(): DateTimeImmutable
     {
         return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(DateTimeImmutable $updatedAt): AbstractBaseEntity
+    {
+        $this->updatedAt = $updatedAt;
+        return $this;
     }
 }

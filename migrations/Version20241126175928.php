394967aa5ee2f5b8e92f25d33a6e3182bb8448fc
Nikita Potorokin <ozornik.xd@mail.ru>
2024-11-26 21:01:31 +0300

    fix: migrations

--- migrations/Version20241126175928.php
+++ migrations/Version20241126175928.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241122215111 extends AbstractMigration
+final class Version20241126175928 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -55,6 +55,21 @@ final class Version20241122215111 extends AbstractMigration
         $this->addSql('COMMENT ON COLUMN products.id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN products.created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('COMMENT ON COLUMN products.updated_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE roles (id UUID NOT NULL, slug VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_B63E2EC75E237E06 ON roles (name)');
+        $this->addSql('COMMENT ON COLUMN roles.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN roles.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN roles.updated_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE users (id UUID NOT NULL, promo_id UUID DEFAULT NULL, password VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, phone INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN users.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN users.promo_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN users.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN users.updated_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE roles_users (user_id UUID NOT NULL, role_id UUID NOT NULL, PRIMARY KEY(user_id, role_id))');
+        $this->addSql('CREATE INDEX IDX_3D80FB2CA76ED395 ON roles_users (user_id)');
+        $this->addSql('CREATE INDEX IDX_3D80FB2CD60322AC ON roles_users (role_id)');
+        $this->addSql('COMMENT ON COLUMN roles_users.user_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN roles_users.role_id IS \'(DC2Type:uuid)\'');
         $this->addSql('ALTER TABLE cart_products ADD CONSTRAINT FK_2D251531A76ED395 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE cart_products ADD CONSTRAINT FK_2D2515314584665A FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE order_product ADD CONSTRAINT FK_2530ADE68D9F6D38 FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
@@ -62,6 +77,8 @@ final class Version20241122215111 extends AbstractMigration
         $this->addSql('ALTER TABLE orders ADD CONSTRAINT FK_E52FFDEEA76ED395 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE orders ADD CONSTRAINT FK_E52FFDEEA09B36F6 FOREIGN KEY (status_slug) REFERENCES order_statuses (slug) ON DELETE RESTRICT NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE orders ADD CONSTRAINT FK_E52FFDEEBF712F8 FOREIGN KEY (delivery_type_slug) REFERENCES delivery_types (slug) ON DELETE RESTRICT NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE roles_users ADD CONSTRAINT FK_3D80FB2CA76ED395 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE roles_users ADD CONSTRAINT FK_3D80FB2CD60322AC FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
@@ -75,11 +92,16 @@ final class Version20241122215111 extends AbstractMigration
         $this->addSql('ALTER TABLE orders DROP CONSTRAINT FK_E52FFDEEA76ED395');
         $this->addSql('ALTER TABLE orders DROP CONSTRAINT FK_E52FFDEEA09B36F6');
         $this->addSql('ALTER TABLE orders DROP CONSTRAINT FK_E52FFDEEBF712F8');
+        $this->addSql('ALTER TABLE roles_users DROP CONSTRAINT FK_3D80FB2CA76ED395');
+        $this->addSql('ALTER TABLE roles_users DROP CONSTRAINT FK_3D80FB2CD60322AC');
         $this->addSql('DROP TABLE cart_products');
         $this->addSql('DROP TABLE delivery_types');
         $this->addSql('DROP TABLE order_product');
         $this->addSql('DROP TABLE order_statuses');
         $this->addSql('DROP TABLE orders');
         $this->addSql('DROP TABLE products');
+        $this->addSql('DROP TABLE roles');
+        $this->addSql('DROP TABLE users');
+        $this->addSql('DROP TABLE roles_users');
     }
 }
